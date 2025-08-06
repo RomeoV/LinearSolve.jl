ffe37384714af271c91d7ef6441004c235aa7e23
Romeo Valentin <romeov@stanford.edu>
2025-08-06 16:48:06 -0700

    Remove all MKL mentions

--- src/LinearSolve.jl
+++ src/LinearSolve.jl
@@ -43,17 +43,18 @@ import Krylov
 
 const CRC = ChainRulesCore
 
-@static if Sys.ARCH === :x86_64 || Sys.ARCH === :i686
-    if Preferences.@load_preference("LoadMKL_JLL",
-        !occursin("EPYC", Sys.cpu_info()[1].model))
-        using MKL_jll
-        const usemkl = MKL_jll.is_available()
-    else
-        const usemkl = false
-    end
-else
-    const usemkl = false
-end
+# @static if Sys.ARCH === :x86_64 || Sys.ARCH === :i686
+#     if Preferences.@load_preference("LoadMKL_JLL",
+#         !occursin("EPYC", Sys.cpu_info()[1].model))
+#         using MKL_jll
+#         const usemkl = MKL_jll.is_available()
+#     else
+#         const usemkl = false
+#     end
+# else
+#     const usemkl = false
+# end
+const usemkl = false
 
 @reexport using SciMLBase
 
@@ -154,7 +155,7 @@ include("common.jl")
 include("extension_algs.jl")
 include("factorization.jl")
 include("appleaccelerate.jl")
-include("mkl.jl")
+# include("mkl.jl")
 include("simplelu.jl")
 include("simplegmres.jl")
 include("iterative_wrappers.jl")
