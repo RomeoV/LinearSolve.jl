c03f128882a4080565f93e45d8cb337389b0d552
Romeo Valentin <romeov@stanford.edu>
2025-08-06 17:01:39 -0700

    Remove MKL_jll dep

--- src/LinearSolve.jl
+++ src/LinearSolve.jl
@@ -43,17 +43,7 @@ import Krylov
 
 const CRC = ChainRulesCore
 
-@static if Sys.ARCH === :x86_64 || Sys.ARCH === :i686
-    if Preferences.@load_preference("LoadMKL_JLL",
-        !occursin("EPYC", Sys.cpu_info()[1].model))
-        using MKL_jll
-        const usemkl = MKL_jll.is_available()
-    else
-        const usemkl = false
-    end
-else
-    const usemkl = false
-end
+const usemkl = false
 
 @reexport using SciMLBase
 
